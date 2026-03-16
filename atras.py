c13d6e808bfbadee00b47b3cb546b21044ada284
JHON TERREROS <alexander05jtv@gmail.com>
2026-03-16 10:16:27 -0500

    Primer commit - ejercicio historial recursivo

---
+++ atras.py
@@ -0,0 +1,37 @@
+def retroceder(historial, pasos):
+
+    #Caso base
+    if pasos == 0 or len(historial) == 0:
+        return historial
+
+    ultima_pagina = historial[-1]
+
+    #Si aparece un Error 404 se detiene el proceso
+    if "Error 404" in ultima_pagina:
+        print("Se encontró un Error 404. Retroceso detenido.")
+        return historial
+
+    print("Retrocediendo desde:", ultima_pagina)
+
+    #Elimina la última página visitada
+    historial.pop()
+
+   
+    return retroceder(historial, pasos - 1)
+
+
+#Historial
+mi_navegacion = [
+    "google.com",
+    "uniminuto.edu",
+    "Error 404: Campus Virtual",
+    "github.com",
+    "stackoverflow.com"
+]
+
+#Número de pasos a retroceder
+pasos = 4
+
+resultado = retroceder(mi_navegacion, pasos)
+
+print("Historial final:", resultado)
